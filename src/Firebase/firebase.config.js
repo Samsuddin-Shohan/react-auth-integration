075ef6589393a5133f8a99c91af3e41ba98c500c
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-09 14:22:40 +0600

    firebase config added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCkVItsGMCrO8_HfQlh3ykU43oC_X0o8lg",
+    authDomain: "react-auth-integration-9d3ef.firebaseapp.com",
+    projectId: "react-auth-integration-9d3ef",
+    storageBucket: "react-auth-integration-9d3ef.appspot.com",
+    messagingSenderId: "225282364892",
+    appId: "1:225282364892:web:eff5c8a3e9ec0fcc182b19",
+    measurementId: "G-5YM60X2QWN"
+  };
+export default firebaseConfig;  
