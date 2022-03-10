0049e9e9c7099b7064e72c718518204a811abf75
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-10 09:36:05 +0600

    private route added

--- src/App.js
+++ src/App.js
@@ -6,24 +6,32 @@ import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
 import Header from './Components/Header/Header';
+import AuthProvider from './context/AuthProvider';
+import Shipping from './Components/Shipping/Shipping';
+import PrivateRoute from './Components/PrivateRouter/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
+     
       <BrowserRouter>
-        <Header></Header>
-        <Switch>
-          <Route path='/home'>
-            <Home></Home>
-          </Route>
-          <Route path='/login'>
-            <Login></Login>
-          </Route>
-          <Route path='/register'>
-            <Register></Register>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Header></Header>
+      <Switch>
+        <Route path='/home'>
+          <Home></Home>
+        </Route>
+        <Route path='/login'>
+          <Login></Login>
+        </Route>
+        <Route path='/register'>
+          <Register></Register>
+        </Route>
+        <PrivateRoute path='/shipping'>
+          <Shipping></Shipping>
+        </PrivateRoute>
+      </Switch>
+    </BrowserRouter>
+      
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import useFirebase from '../../hooks/useFirebase';
 import './Header.css';
 
 const Header = () => {
-    const {user,logOut} = useFirebase();
+    const {user,logOut} = useAuth();
     return (
         <div className='header'>
            <Link to='/home' className='link'>Home</Link>
+           <Link to='/shipping' className='link'>Shipping</Link>
            <Link to='/login' className='link'>Login</Link>
            <Link to='/register' className='link'>Registration</Link>
             <h2 className='user-name'> {user.email}</h2>
